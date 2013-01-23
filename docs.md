bfaa4ebffdcde093b3923d4f30d456eb0a35f74e
scott <scott@sboss.net>
2013-01-23 00:14:15 -0500

    updated ruby for rvm. updated docs.md

--- docs.md
+++ docs.md
@@ -3,10 +3,14 @@
 
 git.rb
 
+determines if you have git installed or not (path dependent).
+
 	has_git = { TRUE, FALSE }
 	git = { *path_to_git* }
 
 perl.rb
+
+determines if you have perl installed or not (path dependent).
 
 	has_perl = { TRUE, FALSE }
 	perl = { *path_to_perl* }
@@ -14,11 +18,17 @@ perl.rb
 
 python.rb
 
+determines if you have python installed or not (path dependent).
+
 	has_python = { TRUE, FALSE }
 	python = { *path_to_python* }
 
 
 ruby.rb
 
+determines if you have ruby installed or not (path dependent).  if you have ruby installed it will check to see if you have rvm installed (path dependent).
+
 	has_ruby = { TRUE, FALSE }
+	has_rvm = { TRUE, FALSE }
 	ruby = { *path_to_ruby* }
+	rvm = { *path_to_rvm* }
--- ruby.rb
+++ ruby.rb
@@ -2,6 +2,7 @@ require 'facter'
 
 # count is used to determine if we have ruby or not.
 count=0
+rvmcount=0
 # 
 # checking the path for ruby
 for p in ENV[ 'PATH' ].split(':')
@@ -21,4 +22,27 @@ if count > 0 then
   Facter.add(:has_ruby) { setcode { "TRUE" } }
 else
   Facter.add(:has_ruby) { setcode { "FALSE" } }  
+end
+
+# determine if we have RVM or not.
+if count > 0 then
+  for p in ENV[ 'PATH' ].split(':')
+    p = p + "/rvm"
+    if File.exists?(p) then
+      Facter.add(:rvm) { setcode { p } }
+      rvmcount=rvmcount+1
+      break
+    else
+      # not found.
+    end
+  
+  end
+
+  # determining if we have RVM or not.
+  if rvmcount > 0 then
+    Facter.add(:has_rvm) { setcode { "TRUE" } }
+  else
+    Facter.add(:has_rvm) { setcode { "FALSE" } }  
+  end
+  
 end
